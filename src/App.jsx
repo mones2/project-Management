bfeee59ce42604bf0c2af382fe91630b241312b9
mones2 <monesgharaibeh@gmail.com>
2025-11-20 11:56:53 +0300

    add sidebar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <main className="h-screen my-8">
+
+      <Sidebar />
+
+    </main>
+  );
+}
+
+export default App;
